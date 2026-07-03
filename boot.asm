e3bd5fdf07cdbd330b59196e4342dfad226cb7e1
Rosan <rosan.syal@gmail.com>
2026-07-03 18:45:50 -0400

    added explanatory comments for my understand lol

--- boot.asm
+++ boot.asm
@@ -1,4 +1,4 @@
-jmp $
+jmp $ ; essentially an infinite loop. constantly jump to current address
 
-times 510-($-$$) db 0
-db 0x55, 0xaa
+times 510-($-$$) db 0 ; add 508 btyes of zeroes so that boot is 512 bytes in total
+db 0x55, 0xaa ; boot signature
