35c46414e2498a42726ecddaafb0948bae9379b3
Rosan <rosan.syal@gmail.com>
2026-07-03 23:18:54 -0400

    learning to display output

--- boot.asm
+++ boot.asm
@@ -1,3 +1,15 @@
+mov bx, 5
+cmp bx, 5
+je loop
+jmp $
+
+loop:
+    mov ah, 0x0e
+    mov al, 65
+    int 0x10
+
+jmp $
+
 jmp $ ; essentially an infinite loop. constantly jump to current address
 
 times 510-($-$$) db 0 ; add 508 btyes of zeroes so that boot is 512 bytes in total
