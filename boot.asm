8adb1d3493d4f0ad732a5281846b58e21b5b56c7
Rosan <rosan.syal@gmail.com>
2026-07-04 20:57:04 -0400

    created a loop to systematically print the alphabet

--- boot.asm
+++ boot.asm
@@ -1,14 +1,19 @@
-mov bx, 5
-cmp bx, 5
-je loop
-jmp $
+;booting once puts us in real mode: 16-bit mode that allows direct memory access   
+
+mov ah, 0x0e ;switches to teletype mode (bios video routine)
+mov al, "A"
+int 0x10 ;call bios interrupt for video routine
 
 loop:
-    mov ah, 0x0e
-    mov al, 65
+    inc al
+    cmp al, "Z" + 1
+    je exit
     int 0x10
+    jmp loop
 
-jmp $
+exit:
+    jmp $
+
 
 jmp $ ; essentially an infinite loop. constantly jump to current address
 
