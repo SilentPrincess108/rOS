738a45ccbca0d11fdcec4f2ceca285be150518a2
Rosan <rosan.syal@gmail.com>
2026-07-06 13:51:37 -0400

    practice for outputting strings

--- boot.asm
+++ boot.asm
@@ -3,11 +3,10 @@
 
 mov ah, 0x0e ;switches to teletype mode (bios video routine)
 mov bx, quote ;move this point in memory into bx
-int 0x10 ;call bios interrupt for video routine
 
 printLoop:
     mov al, [bx] ;move whatever is at bx, into al
-    int 0x10
+    int 0x10 ;call bios interrupt for video routine
     cmp al, 0
     je exit
     inc bx
@@ -17,7 +16,7 @@ exit:
     jmp $ ; essentially an infinite loop. constantly jump to current address
 
 quote:
-    db "It's dangerous to go alone. Here, take this: ", 0
+    db "Anything for queen roro", 0
 
 times 510-($-$$) db 0 ; add 508 btyes of zeroes so that boot is 512 bytes in total
 db 0x55, 0xaa ; boot signature
